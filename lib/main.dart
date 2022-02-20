ecf49fa5e4b3d6874acebc580fe695e5fb79ab08
dansp <dsp102009@hotmail.com>
2022-02-20 12:57:26 -0300

    fix: animation added

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool selected = false;
 
   @override
   Widget build(BuildContext context) {
@@ -38,14 +39,29 @@ class _MyHomePageState extends State<MyHomePage> {
         color: const Color(0xFF005F40),
         child: Center(
 
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset('assets/logo/Original_small.png', width: 400),
-              const Text(
-                'UNDER CONSTRUCTION', style: TextStyle(color: Colors.white, fontSize: 23),
+          child: InkWell(
+            onTap: () {
+              setState(() {
+                selected = !selected;
+              });
+            },
+            child: AnimatedContainer(
+              width: selected ? 300.0 : 400.0,
+              height: selected ? 200.0 : 400.0,
+              alignment:
+              selected ? Alignment.center : AlignmentDirectional.topCenter,
+              duration: const Duration(seconds: 2),
+              curve: Curves.fastOutSlowIn,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('assets/logo/Original_small.png', width: 400),
+                  selected ? const Text(
+                    'UNDER CONSTRUCTION', style: TextStyle(color: Colors.white, fontSize: 23),
+                  ) : Container(),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
